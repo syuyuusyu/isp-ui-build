73bcd410a239b39fb2d1a85f3d5f77aa9eb55c5e
dengrongtao <y295483713@qq.com>
2018-06-01 09:43:39 +0800

    用户管理里面使机构换行

--- src/userRole/store.js
+++ src/userRole/store.js
@@ -1,180 +1,193 @@
-import {observable, configure,action,runInAction,} from 'mobx';
-import {notification,Button,Popconfirm} from 'antd';
-import {baseUrl,get,post} from '../util';
+import {observable, configure, action, runInAction,} from 'mobx';
+import {notification, Button, Popconfirm} from 'antd';
+import {baseUrl, get, post} from '../util';
 import React from 'react';
 import RoleButton from '../roleButton';
 import {Divider,} from 'antd';
 
 
-configure({ enforceActions: true });
+configure({enforceActions: true});
 
-export class UserRoleStore{
+export class UserRoleStore {
 
-    constructor(rootStore){
-        this.rootStore=rootStore;
-    }
+  constructor(rootStore) {
+    this.rootStore = rootStore;
+  }
 
-    @observable
-    targetKeys=[];
+  @observable
+  targetKeys = [];
 
-    @observable
-    currentUserId=-1;
+  @observable
+  currentUserId = -1;
 
-    @observable
-    selectedKeys=[];
+  @observable
+  selectedKeys = [];
 
-    @observable
-    userRoleConfVisible=false;
+  @observable
+  userRoleConfVisible = false;
 
-    @observable
-    users=[];
+  @observable
+  users = [];
 
-    @observable
-    userType='1';
+  @observable
+  userType = '1';
 
-    @observable
-    userRoleConfRoles=[];
+  @observable
+  userRoleConfRoles = [];
 
-    @observable
-    columns=[
-        // {dataIndex:'belong',title:'所属系统',width:100},
-        {dataIndex:'user_name',title:'登录名称',width:100},
-        {dataIndex:'name',title:'用户名称',width:100},
-        {dataIndex:'phone',title:'电话',width:100},
-        {dataIndex:'email',title:'EAIL',width:100},
-        {dataIndex:'organization_name',title:'所属机构',width:150},
-        {
-            title: '操作',
-            width: 200,
-            render: (text, record) => {
-                return (
-                    <span>
+  @observable
+  columns = [
+    // {dataIndex:'belong',title:'所属系统',width:100},
+    {dataIndex: 'user_name', title: '登录名称', width: 100},
+    {dataIndex: 'name', title: '用户名称', width: 100},
+    {dataIndex: 'phone', title: '电话', width: 100},
+    {dataIndex: 'email', title: 'EAIL', width: 100},
+    {
+      dataIndex: 'organization_name', title: '所属机构', width: 150,
+      render: (orgName) => {
+        let orgNameArray = orgName.split(';');
+        orgNameArray.length = orgNameArray.length - 1;
+        let newOrgNameArray=[];
+        if (orgNameArray.length >= 0) {
+          for (let i=0;i<orgNameArray.length;i++) {
+             newOrgNameArray.push(<p key={i}>{orgNameArray[i]}</p>)
+          }
+        }
+        //let orgNameString=newOrgNameArray.join('');
+        console.log("newOrgNameArray的值为:",newOrgNameArray,newOrgNameArray.length);
+        return  newOrgNameArray;
+      }
+    },
+    {
+      title: '操作',
+      width: 200,
+      render: (text, record) => {
+        return (
+          <span>
                         {/*<Button icon="profile" onClick={this.props.rootStore.userRoleStore.userRoleConf(record)} size='small'>用户角色配置</Button>*/}
-                        <RoleButton buttonId={12} onClick={this.userRoleConf(record)}/>
+            <RoleButton buttonId={12} onClick={this.userRoleConf(record)}/>
                         <Divider type="vertical"/>
                       <Popconfirm onConfirm={this.resetPassword(record)} title="确认重置?">
                         <RoleButton icon="reload" buttonId={30} onClick={null} size='small'>重置密码</RoleButton>
                       </Popconfirm>
 
-                        {/*<Popconfirm onConfirm={null} title="确认删除?">*/}
-                            {/*/!*<Button icon="delete" onClick={null} size='small'>删除</Button>*!/*/}
-                            {/*<RoleButton buttonId={13}/>*/}
-                        {/*</Popconfirm>*/}
+            {/*<Popconfirm onConfirm={null} title="确认删除?">*/}
+            {/*/!*<Button icon="delete" onClick={null} size='small'>删除</Button>*!/*/}
+            {/*<RoleButton buttonId={13}/>*/}
+            {/*</Popconfirm>*/}
                     </span>
-                )
-            }
-        }
-    ];
-
-    @action
-    loadUserRoleConfRoles=async ()=>{
-        //userRoleConfRoles
-        //let json=await get(`${baseUrl}/user/userRoleConfRoles/${this.currentUserId}`) ;
-        let json=await get(`${baseUrl}/role/allRoles`);
-        runInAction(()=>{
-            this.userRoleConfRoles=json;
-        });
-    };
-
-
-    @action
-    loadAllUsers=async ()=>{
-       let json=await get(`${baseUrl}/user/allUsers`) ;
-       runInAction(()=>{
-          this.users=json;
-       });
-    };
-
-    @action
-    loadCurrentUserRole=async ()=>{
-        let json=await get(`${baseUrl}/role/userRole/${this.currentUserId}`) ;
-        runInAction(()=>{
-            this.targetKeys=json.map(r=>r.id);
-        });
-    };
-
-
-
-    @action
-    taggreUserRoleConf=()=>{
-       this.userRoleConfVisible=!this.userRoleConfVisible;
-    };
-
-    @action
-    userRoleConf=(record)=>(()=>{
-        this.currentUserId=record.id;
-        this.taggreUserRoleConf();
-    });
-
-
-    @action
-    handleChange = (nextTargetKeys, direction, moveKeys) => {
-        console.log('handleChange');
-        this.targetKeys=nextTargetKeys;
-
-    };
-
-    @action
-    handleSelectChange = (sourceSelectedKeys, targetSelectedKeys) => {
-        console.log('handleSelectChange');
-        this.selectedKeys=[...sourceSelectedKeys, ...targetSelectedKeys];
-    };
-
-    @action
-    handleScroll = (direction, e) => {
-        console.log('direction:', direction);
-        console.log('target:', e.target);
-    };
-
-    saveUserRole=async ()=>{
-        let json=await post(`${baseUrl}/user/saveUserRole`,{userId:this.currentUserId,roleIds:this.targetKeys});
-         if(json.success){
-             notification.success({
-                 message:'保存成功',
-             })
-         }else{
-             notification.error({
-                 message:'后台错误，请联系管理员',
-             })
-         }
-         this.taggreUserRoleConf();
-
-    };
-
-    @action
-    handleReset=()=>{
-        this.targetKeys=[];
-    };
-
-
-
-    @action
-    userTypeChange=(v)=>{
-        this.userType=v;
-        if(v==='1'){
-            this.columns[0]={dataIndex:'belong',title:'所属系统',width:100};
-        }
-        if(v==='2'){
-            this.columns[0]={dataIndex:'belong',title:'所属机构',width:100};
-        }
-        this.loadAllUsers();
-
-    };
-
-    @action
-    resetPassword= (record)=>(async ()=>{
-      const userName=record.user_name;
-      let json=await get(`${baseUrl}/resetPassword/${userName}`) ;
-      if(json.success){
-        notification.success({
-          message:'保存成功',
-        })
-      }else{
-        notification.error({
-          message:'后台错误，请联系管理员',
-        })
+        )
       }
-    })
+    }
+  ];
+
+  @action
+  loadUserRoleConfRoles = async () => {
+    //userRoleConfRoles
+    //let json=await get(`${baseUrl}/user/userRoleConfRoles/${this.currentUserId}`) ;
+    let json = await get(`${baseUrl}/role/allRoles`);
+    runInAction(() => {
+      this.userRoleConfRoles = json;
+    });
+  };
+
+
+  @action
+  loadAllUsers = async () => {
+    let json = await get(`${baseUrl}/user/allUsers`);
+    runInAction(() => {
+      this.users = json;
+    });
+  };
+
+  @action
+  loadCurrentUserRole = async () => {
+    let json = await get(`${baseUrl}/role/userRole/${this.currentUserId}`);
+    runInAction(() => {
+      this.targetKeys = json.map(r => r.id);
+    });
+  };
+
+
+  @action
+  taggreUserRoleConf = () => {
+    this.userRoleConfVisible = !this.userRoleConfVisible;
+  };
+
+  @action
+  userRoleConf = (record) => (() => {
+    this.currentUserId = record.id;
+    this.taggreUserRoleConf();
+  });
+
+
+  @action
+  handleChange = (nextTargetKeys, direction, moveKeys) => {
+    console.log('handleChange');
+    this.targetKeys = nextTargetKeys;
+
+  };
+
+  @action
+  handleSelectChange = (sourceSelectedKeys, targetSelectedKeys) => {
+    console.log('handleSelectChange');
+    this.selectedKeys = [...sourceSelectedKeys, ...targetSelectedKeys];
+  };
+
+  @action
+  handleScroll = (direction, e) => {
+    console.log('direction:', direction);
+    console.log('target:', e.target);
+  };
+
+  saveUserRole = async () => {
+    let json = await post(`${baseUrl}/user/saveUserRole`, {userId: this.currentUserId, roleIds: this.targetKeys});
+    if (json.success) {
+      notification.success({
+        message: '保存成功',
+      })
+    } else {
+      notification.error({
+        message: '后台错误，请联系管理员',
+      })
+    }
+    this.taggreUserRoleConf();
+
+  };
+
+  @action
+  handleReset = () => {
+    this.targetKeys = [];
+  };
+
+
+  @action
+  userTypeChange = (v) => {
+    this.userType = v;
+    if (v === '1') {
+      this.columns[0] = {dataIndex: 'belong', title: '所属系统', width: 100};
+    }
+    if (v === '2') {
+      this.columns[0] = {dataIndex: 'belong', title: '所属机构', width: 100};
+    }
+    this.loadAllUsers();
+
+  };
+
+  @action
+  resetPassword = (record) => (async () => {
+    const userName = record.user_name;
+    let json = await get(`${baseUrl}/resetPassword/${userName}`);
+    if (json.success) {
+      notification.success({
+        message: '保存成功',
+      })
+    } else {
+      notification.error({
+        message: '后台错误，请联系管理员',
+      })
+    }
+  })
 
 
 }
