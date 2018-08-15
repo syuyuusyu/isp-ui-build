ba6045e144304c1397319f4082f5b8d8c26dc84f
dengrongtao <295483713@qq.com>
2018-08-15 15:00:19 +0800

    oracle用户列表界面用户按时间排序，新增用户查询功能

--- src/oracleUser/oracleUserTable.js
+++ src/oracleUser/oracleUserTable.js
@@ -20,7 +20,7 @@ class OracleUserTable extends React.Component{
     {dataIndex:'tempTbs',title:'临时表空间',width:100},
     {dataIndex:'account_status',title:'是否可用',width:100, render:(account_status)=>{return account_status==='OPEN'?'是':'否'}},
     {dataIndex:'instanceName',title:'所属实例',width:100},
-    {dataIndex:'created',title:'创建时间',width:100},
+    {dataIndex:'created',title:'创建时间',width:100, sorter:(a,b)=>Date.parse(b.created)-Date.parse(a.created)},
   ];
 
  render(){
@@ -32,10 +32,23 @@ class OracleUserTable extends React.Component{
            <Col span={4} style={{ textAlign: 'right' }} className="col-button">
              <Icon type="profile" />&nbsp;&nbsp; <Link to="/createOracleUser">新建用户</Link>
            </Col>
+           <Col span={2} offset={0} style={{lineHeight:'32px'}}>用户名:</Col>
+           <Col span={3} offset={1}>
+             <Select style={{width:150}}  onChange={store.setQueryOracleUser}>
+               <Option  value={''} style={{color:'white'}}>&nbsp;</Option>
+               {
+                 store.allOracleUserBack.filter(d=>d).map(s=>
+                   <Option key={s.username} value={s.username}>{s.username}</Option>)
+               }
+             </Select>
+           </Col>
+           <Col span={4}>
+             <Button icon="search" onClick={store.loadQueryOracleUser}>查询</Button>
+           </Col>
          </Row>
          <Table columns={this.columns}
                 rowKey={record => record.user_id}
-                dataSource={store.allOracleUser.filter(d=>d)}
+                dataSource={store.allOracleUser.filter(d=>d).sort((a,b)=>Date.parse(b.created)-Date.parse(a.created))}
                 rowSelection={null}
                 size="small"
                 scroll={{ y: 800 }}
